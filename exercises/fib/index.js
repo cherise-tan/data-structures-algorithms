617b25acc14a35505203327fdac35aad572734a2
Cherise <cherisetan@live.com>
2019-05-29 15:38:16 +1200

    Solve fibonacci iterative

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
@@ -8,6 +10,19 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Iterative solution
+function fib(n) {
+  const result = [0, 1];
+
+  for (i = 2; i <= n; i++){
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+  return result[n];
+}
+
+
 
 module.exports = fib;
